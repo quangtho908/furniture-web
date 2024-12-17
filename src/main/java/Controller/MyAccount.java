122a2bd540597856360c5179f119644fdd982da3
LH-210402 <luonghqhuy@gmail.com>
2024-12-17 23:39:32 +0700

    update noti in personal setting

--- src/main/java/Controller/MyAccount.java
+++ src/main/java/Controller/MyAccount.java
@@ -3,8 +3,10 @@ package Controller;
 import DTO.AuthorizationData;
 import DTO.UpdateUserDTO;
 import Model.StatusAccount;
+import Services.KeyService;
 import Services.UserService;
-
+import Model.User;
+import Model.Keys;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -13,16 +15,19 @@ import java.io.IOException;
 @WebServlet(name = "MyAccount", value = "/myAccount")
 public class MyAccount extends HttpServlet {
     private UserService userService;
+    private KeyService keyService;
 
     @Override
     public void init() throws ServletException {
         super.init();
         this.userService = new UserService("users");
+        this.keyService = new KeyService("keys");
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         boolean logged = (boolean) request.getAttribute("logged");
+      HttpSession session = request.getSession(true);
         if(!logged) {
             response.sendRedirect("/");
         }else {
@@ -36,6 +41,14 @@ public class MyAccount extends HttpServlet {
                 request.setAttribute("success", "Thay đổi mật khẩu thành công");
             }else if(success.equals("changeInfo")){
                 request.setAttribute("success", "Cập nhật thông tin thành công");
+            }else if(success.equals("addKey")) {
+                request.setAttribute("success", "Key đã được update");
+            }
+            AuthorizationData data = (AuthorizationData) session.getAttribute("authorization");
+            User user = this.userService.findById(data.getId(), User.class);
+            if(user.getPublicKey() > 0) {
+              Keys key = this.keyService.findById(String.valueOf(user.getPublicKey()), Keys.class);
+              request.setAttribute("publicKey", key.getValue());
             }
 
             request.getRequestDispatcher("/jsp/client/myAccount.jsp").forward(request, response);
