7f050537d1dac2a2f250351751a569fddd77ca4e
quangtho908 <quangtho23062002@gmail.com>
2024-12-23 10:26:00 +0700

    send mail update key

--- src/main/java/Controller/KeyController.java
+++ src/main/java/Controller/KeyController.java
@@ -1,8 +1,10 @@
 package Controller;
 
 import DTO.AuthorizationData;
+import Model.MailContent;
 import Model.User;
 import Services.KeyService;
+import Services.MailService;
 import Services.UserService;
 
 import javax.servlet.ServletException;
@@ -13,17 +15,21 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import Model.Keys;
+import com.mailjet.client.errors.MailjetException;
+import com.mailjet.client.errors.MailjetSocketTimeoutException;
 
 @WebServlet(name = "KeyController", value = "/keys")
 public class KeyController extends HttpServlet {
   private KeyService keyService;
   private UserService userService;
+  private MailService mailService;
 
   @Override
   public void init() throws ServletException {
     super.init();
     this.keyService = new KeyService("keys");
     this.userService = new UserService("users");
+    this.mailService = new MailService();
   }
 
   @Override
@@ -38,13 +44,21 @@ public class KeyController extends HttpServlet {
       response.sendRedirect("/login");
       return;
     }
+
     HttpSession session = request.getSession(true);
     AuthorizationData authorizationData = (AuthorizationData) session.getAttribute("authorization");
+    User user = this.userService.findById(authorizationData.getId(), User.class);
     String key = request.getParameter("publicKey");
     Keys publicKey = new Keys(key, authorizationData.getId());
     String keyId = this.keyService.create(publicKey);
 
     this.userService.updatePublicKey(Integer.parseInt(keyId), authorizationData.getId());
+    MailContent mailContent = new MailContent("FURNITURE Thay đổi mã xác thực", "Tài khoản của bạn đã thay đổi mã xác thực");
+    try {
+      this.mailService.send(user.getEmail(), mailContent);
+    } catch (MailjetSocketTimeoutException | MailjetException e) {
+      throw new RuntimeException(e);
+    }
     response.sendRedirect("/myAccount?success=addKey");
     return;
 
